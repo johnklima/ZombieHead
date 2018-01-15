2313e44b4d5839bccd8bc6db508b87392fe84a20
johnklima <klima.de.sa@gmail.com>
2018-01-15 11:40:31 +0100

    added reference files for gravity and camera

---
+++ ZombieHead/Assets/followShip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followShip : MonoBehaviour {
+
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //force the camera to follow the ship
+        transform.position = target.position + new Vector3(0, 3, -4);
+        transform.LookAt(target);
+
+    }
+}
