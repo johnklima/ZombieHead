ee2bdd8a03996e86c3a2ea5a23880760cbf78830
Benjamin H <Behammar96@gmail.com>
2018-01-23 19:17:12 +0100

    Vertical_Platform

---
+++ ZombieHead/Assets/PlatformMovment.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovment : MonoBehaviour
+{
+    private Vector3 posA;
+
+    private Vector3 posB;
+
+    private Vector3 nexPos;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Transform ChildTransform;
+
+    [SerializeField]
+    private Transform transformB;
+
+	// Use this for initialization
+	void Start () {
+        posA = ChildTransform.localPosition;
+        posB = transformB.localPosition;
+        nexPos = posB;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+	}
+
+    private void Move()
+    {
+        ChildTransform.localPosition = Vector3.MoveTowards(ChildTransform.localPosition,nexPos,speed * Time.deltaTime);
+
+        if (Vector3.Distance(ChildTransform.localPosition,nexPos) <= 0.1)
+        {
+            ChangeDestination();
+        }
+    }
+
+    private void ChangeDestination()
+    {
+        nexPos = nexPos != posA ? posA : posB;
+    }
+}
