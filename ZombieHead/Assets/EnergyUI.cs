f8224f5f2a7138b0db58de4110e5f392c28a8d0a
Alexander B <alexander_birkeland@hotmail.com>
2018-01-24 20:30:28 +0100

    Add script for updating text and/or slider for energy.
    Hopefully won't break anything. Until I can get the latest build to work, I don't know how to also include the text and slider objects, but they will be easy to remake once I do solve the issue. Also: If Consumption == 0, then Energy remains constant (so use a non-0 Consumption to test it. I used 0.1)

---
+++ ZombieHead/Assets/EnergyUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyUI : MonoBehaviour
+{
+    public Text energyText; // The energy remaining.
+    public Slider speedSlider;
+    float motionEnergy;
+    public PlayerMotion playerMotion;
+
+    // Use this for initialization
+    void Start()
+    {
+        motionEnergy = 0; // Currently redundant, but I'm expecting that we'll want to truncate it, and potentially do math with it first.
+        energyText.text = "Energy Full";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        motionEnergy = playerMotion.energy;
+        energyText.text = "Energy Remaining: " + motionEnergy; // Not part of the request, but was useful during debugging and I'll leave it until we know more about how the design will look. Possibly add '+ maxEnergy' (after defining that) if we want the max to change.
+        speedSlider.value = motionEnergy;
+    }
+}
