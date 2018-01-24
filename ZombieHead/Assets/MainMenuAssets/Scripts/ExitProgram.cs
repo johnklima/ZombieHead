af6649758261376de3b874a00f209d85399783be
Myaskill <magnus.enar.myrvang@gmail.com>
2018-01-24 06:56:06 +0100

    Main Menu
    Main Menu completed, with level selection built in. Options are placeholders for now.

---
+++ ZombieHead/Assets/MainMenuAssets/Scripts/ExitProgram.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitProgram : MonoBehaviour {
+
+	void doExitGame() 
+	{
+		Debug.Log ("We're quitting the game now!");
+		Application.Quit();
+	}
+}
