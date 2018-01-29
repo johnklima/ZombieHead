845c2857200c9fe7bad77703aa71b559368f7dce
Benjamin H <Behammar96@gmail.com>
2018-01-29 15:50:14 +0100

    light

---
+++ ZombieHead/Assets/Scripts/BlinkingLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BlinkingLight : MonoBehaviour
+{
+
+    public float Seconds;    
+    public float Intensity;     
+    public Light myLight;
+
+    public IEnumerator GetFlashNow()
+    {
+        float waitTime = Seconds / 2;
+
+        while (myLight.intensity < Intensity)
+        {
+            myLight.intensity += Time.deltaTime / waitTime;
+            yield return null;
+        }
+        while (myLight.intensity > 0)
+        {
+            myLight.intensity -= Time.deltaTime / waitTime;
+            yield return null;
+        }
+        yield return null;
+    }
+}
