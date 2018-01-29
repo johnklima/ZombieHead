3d762139618156b21a5b81146688214ecc49f189
Alexander B <alexander_birkeland@hotmail.com>
2018-01-29 10:22:03 +0100

    WindUI v1.1
    Can easily be persuaded to change several parts of this. But for now, a simple two-bar system with red for -z wind (left), and blue for +z wind (right).

---
+++ ZombieHead/Assets/WindUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindUI : MonoBehaviour
+{
+    // public Text windLabel; // Currently it's just a label. But if we want to make the numbers more transparent we could put raw numbers here.
+    public Slider WindSlider; // z, blue, right
+    public Slider NWindSlider; // -z, red, left
+    float windEnergy; // Positive wind
+    float windEnergyN; // Negative wind
+    public PlayerMotion playerMotion; // Or wherever we get the windForce
+
+    // Use this for initialization
+    void Start()
+    {
+        WindSlider.value = 0;
+        NWindSlider.value = 0;
+        // windLabel.text = "Wind Neutral"; // Whatever we want to have as the starting label.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMotion.windForce.z <= 0) //If the wind needs 'fixing'
+        {
+            WindSlider.value = 0; // Resetting it in case we do sudden jumps.
+            NWindSlider.value = playerMotion.windForce.z * -1;
+        }
+        else //If the wind is already positive.
+        {
+            NWindSlider.value = 0; // Resetting it in case we do sudden jumps.
+            WindSlider.value = playerMotion.windForce.z;
+        }
+        //windLabel.text = "Update text goes here" + playerMotion.windForce.z; // Whatever we want it to update to.
+    }
+}
