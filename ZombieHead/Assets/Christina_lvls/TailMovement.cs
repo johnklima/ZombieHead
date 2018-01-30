d38e00da9c3060e5a0bfc62d0c2a06c0e5a26546
Kammyra <35478235+Kammyra@users.noreply.github.com>
2018-01-30 14:29:38 +0100

    Some massega

--- ZombieHead/Assets/Christina_lvls/TailMovement.cs
+++ ZombieHead/Assets/Christina_lvls/TailMovement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RightEyeTarget : MonoBehaviour {
+public class TailMovement : MonoBehaviour
+{
     public float xamplitude = 1.0f;
     public float xfrequency = 1.0f;
     public float yamplitude = 1.0f;
@@ -11,24 +12,24 @@ public class RightEyeTarget : MonoBehaviour {
     public float zfrequency = 1.0f;
 
     Vector3 startPosition;
-    
-    // Use this for initialization
-    void Start () {
-        
-        startPosition = transform.localPosition;        
-		
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPosition = transform.localPosition;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         float x = Mathf.Sin(Time.time * xfrequency) * xamplitude;
         float y = Mathf.Sin(Time.time * yfrequency) * yamplitude;
         float z = Mathf.Sin(Time.time * zfrequency + Mathf.PI) * zamplitude;
 
         Vector3 pos = Vector3.zero;
-        pos.Set(startPosition.x + x, startPosition.y + y, startPosition.z + z);
+        pos.Set(startPosition.x + x, startPosition.y, startPosition.z);
 
         transform.localPosition = pos;
-		
+
 	}
 }
