5e6b44cd807135a35b9bb9f64c18110ed59ecc34
Benjamin H <Behammar96@gmail.com>
2018-01-30 12:52:20 +0100

    flameflicker

---
+++ ZombieHead/Assets/Scripts/FlameOrange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FlameOrange : MonoBehaviour
+{
+
+    public float timer1;
+
+    private Light lighOrange;
+
+    private float waitTime1 = 1;
+
+    private void Start()
+    {
+        timer1 = Time.time;
+
+        lighOrange = transform.GetComponent<Light>();
+    }
+    private void Update()
+    {
+        if (Time.time - timer1 > waitTime1)
+        {
+            timer1 = Time.time;
+            waitTime1 = Random.RandomRange(0.05f, 0.1f);
+
+            lighOrange.enabled = !lighOrange.enabled;
+
+        }
+
+    }
+
+}
---
+++ ZombieHead/Assets/Scripts/flameRed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FlameRed : MonoBehaviour
+{
+
+    public float timer2;
+
+    private Light lighRed;
+
+    private float waitTime1 = 1;
+
+    private void Start()
+    {
+        timer2 = Time.time;
+
+        lighRed = transform.GetComponent<Light>();
+    }
+    private void Update()
+    {
+        if (Time.time - timer2 > waitTime1)
+        {
+            timer2 = Time.time;
+            waitTime1 = Random.RandomRange(0.05f, 0.1f);
+
+            lighRed.enabled = !lighRed.enabled;
+
+        }
+
+    }
+
+}
